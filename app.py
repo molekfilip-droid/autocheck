2cc2d2fac2c8242541c12cc54d45baec92584f97
molekfilip-droid <molekfilip@gmail.com>
2026-08-21 13:31:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,12 @@ with st.expander("🔍 Zkontrolovat načtenou výbavu", expanded=True):
 
 st.markdown("### 🚗 Parametry vozidla")
 c1, c2 = st.columns(2)
+
 model = c1.text_input("Značka a model", value=st.session_state.form_model)
 year = c2.number_input("Rok výroby", min_value=1990, max_value=2026, value=int(st.session_state.form_year))
 km = c1.number_input("Nájezd (km)", min_value=0, value=int(st.session_state.form_km), step=1000)
-price = c2.number_input("Cena (Kč)", min_value=0, value=int(st.session_state.form_price), step=
+price = c2.number_input("Cena (Kč)", min_value=0, value=int(st.session_state.form_price), step=10000)
+
+f_opts = ["Benzín", "Nafta", "Hybrid", "Elektro"]
+curr_f = st.session_state.get("form_fuel", "Benzín")
+f_index = f
